d9d9ff07996e40a4a5330e2259517edb9843b802
Mischa Reitsma <reitsma.mischa@gmail.com>
2026-01-14 18:46:41 +0100

    Change temp todo script name building test env
    Change the name of todo.sh when adding it to the test directory
    when building the test container. This script will be removed
    when the container stops, and this prevents accidentally removing
    the actual main todo.sh.

--- test/run_test_env.sh
+++ test/run_test_env.sh
@@ -8,9 +8,9 @@ err() {
 # Sub-shell for easy going back to original dir
 (
 	cd "$(dirname "${0}")" || err "Failed to change dir"
-	cp ../todo.sh .
+	cp ../todo.sh todo_docker_add.sh
 	docker build -t shell-todo-test:latest .
 	docker run --rm -ti --user todo --name shell-todo-test shell-todo-test:latest bash
 	docker rmi shell-todo-test:latest
-	rm todo.sh
+	rm todo_docker_add.sh
 )
